57c6df4b048dc4c0cdcfc014e16ffa474e186974
Zach <zmcartor@gmail.com>
2012-09-30 19:54:27 -0400

    Main app changes, CoffeeScript class, new routes
    converted HappieApp to normal CoffeeScript class. Route for card composer

--- public/javascript/app/happie_app.coffee
+++ public/javascript/app/happie_app.coffee
@@ -1,9 +1,15 @@
+#our main namespace
 window.HappieApp or = {}
+
+#our main event bus
+window.HappieApp.EventBus = {}
+_.extend(window.HappieApp.EventBus, Backbone.Events)
 
 window.HappieApp.Router = class Router extends Backbone.Router
   routes:
     'choose_background': 'choose_background',
-    'history' : 'history'
+    'history' : 'history',
+    'compose' : 'compose'
 
   initialize: ()->
    console.log 'created!!!!'
@@ -11,13 +17,43 @@ window.HappieApp.Router = class Router extends Backbone.Router
 
   choose_background: ()->
     console.log 'show_backgrounds'
-    our_view = new HappieApp.Views.Choose_background()
+    @hanger$.empty()
+    our_view = new window.HappieApp.Views.Choose_background()
     @hanger$.append(our_view.el)
+
+  compose: ()->
+    console.log 'compose card'
+    @hanger$.empty()
+    #when a background is chosen, App instance will keep track of it
+    card_compose_view = new window.HappieApp.Views.Card_compose(model: window.App.bg_model)
 
   history: ()->
     console.log 'history!!'
 
-window.HappieApp.App = () ->
-  @router = new window.HappieApp.Router
-  Backbone.history.start();
-  @router.navigate "/choose_background", true
+
+#Our Main APP driver. Regular CoffeeScript Class
+window.HappieApp.App = class HappieApp
+
+  constructor: ()->
+    console.log 'new app!!'
+    @router = new window.HappieApp.Router
+    @selected_background = undefined
+
+    #setup various event listeners
+    window.HappieApp.EventBus.on('backgrounds:selected', @background_chosen)
+    window.HappieApp.EventBus.on('backgrounds:deselected',@background_unchose)
+
+    Backbone.history.start()
+
+  #fat arrow, bind to 'this' and not in context of event handler
+  background_chosen: (@bg_model)=>
+    @selected_background = @bg_model
+    console.log 'chosen bg!'
+    @router.navigate "/compose", true
+
+  background_unchose: ()=>
+    @selected_background = undefined
+    console.log 'unchosen!!'
+
+  start:()->
+    @router.navigate "/choose_background", true
--- public/javascript/app/happie_app.js
+++ public/javascript/app/happie_app.js
@@ -1,9 +1,14 @@
 (function() {
-  var Router,
+  var HappieApp, Router,
     __hasProp = {}.hasOwnProperty,
-    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; };
+    __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; },
+    __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
 
   window.HappieApp || (window.HappieApp = {});
+
+  window.HappieApp.EventBus = {};
+
+  _.extend(window.HappieApp.EventBus, Backbone.Events);
 
   window.HappieApp.Router = Router = (function(_super) {
 
@@ -15,7 +20,8 @@
 
     Router.prototype.routes = {
       'choose_background': 'choose_background',
-      'history': 'history'
+      'history': 'history',
+      'compose': 'compose'
     };
 
     Router.prototype.initialize = function() {
@@ -26,8 +32,18 @@
     Router.prototype.choose_background = function() {
       var our_view;
       console.log('show_backgrounds');
-      our_view = new HappieApp.Views.Choose_background();
+      this.hanger$.empty();
+      our_view = new window.HappieApp.Views.Choose_background();
       return this.hanger$.append(our_view.el);
+    };
+
+    Router.prototype.compose = function() {
+      var card_compose_view;
+      console.log('compose card');
+      this.hanger$.empty();
+      return card_compose_view = new window.HappieApp.Views.Card_compose({
+        model: window.App.bg_model
+      });
     };
 
     Router.prototype.history = function() {
@@ -38,10 +54,38 @@
 
   })(Backbone.Router);
 
-  window.HappieApp.App = function() {
-    this.router = new window.HappieApp.Router;
-    Backbone.history.start();
-    return this.router.navigate("/choose_background", true);
-  };
+  window.HappieApp.App = HappieApp = (function() {
+
+    function HappieApp() {
+      this.background_unchose = __bind(this.background_unchose, this);
+
+      this.background_chosen = __bind(this.background_chosen, this);
+      console.log('new app!!');
+      this.router = new window.HappieApp.Router;
+      this.selected_background = void 0;
+      window.HappieApp.EventBus.on('backgrounds:selected', this.background_chosen);
+      window.HappieApp.EventBus.on('backgrounds:deselected', this.background_unchose);
+      Backbone.history.start();
+    }
+
+    HappieApp.prototype.background_chosen = function(bg_model) {
+      this.bg_model = bg_model;
+      this.selected_background = this.bg_model;
+      console.log('chosen bg!');
+      return this.router.navigate("/compose", true);
+    };
+
+    HappieApp.prototype.background_unchose = function() {
+      this.selected_background = void 0;
+      return console.log('unchosen!!');
+    };
+
+    HappieApp.prototype.start = function() {
+      return this.router.navigate("/choose_background", true);
+    };
+
+    return HappieApp;
+
+  })();
 
 }).call(this);
