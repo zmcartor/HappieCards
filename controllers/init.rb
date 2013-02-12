d18b5e2262c7aaa18d47129f0ef29b3e2e71965a
Zach <zmcartor@gmail.com>
2013-02-11 22:25:56 -0500

    Remove defunt API key now that we're public ;)

--- controllers/init.rb
+++ controllers/init.rb
@@ -2,7 +2,3 @@ require_relative './users_controller'
 require_relative './cards_controller'
 require_relative './backgrounds_controller'
 require_relative './callbacks_controller'
-
-#rackspace cloud API key: 5860606f0f14fbf4d16833e62600379c
-#username zmcartor
-#container name happiecards
