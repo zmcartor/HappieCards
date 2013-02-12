17e12f8653fd250771b3db9f4a0643573c12136c
Zach <zmcartor@gmail.com>
2013-02-11 22:28:30 -0500

    Load paperclip defaults if included

--- models/init.rb
+++ models/init.rb
@@ -18,3 +18,18 @@ config = YAML.load_file('config/db.yml')
 
     #globally don't include the root in json
     ActiveRecord::Base.include_root_in_json = false
+
+    # Load up Paperclip default settings if applicable
+    begin
+      paperclip_defaults = YAML.load_file('config/paperclip.yml')
+      
+      #Format this hash with :symbol indexes, fix this up somehow ... 
+      formatted_hash = paperclip_defaults['defaults'].inject({}){|memo,(k,v)| memo[k.to_sym] = v; memo}
+      formatted_hash[:fog_credentials] = formatted_hash[:fog_credentials].inject({}){|memo,(k,v)| memo[k.to_sym] = v; memo}
+
+      Paperclip::Attachment.default_options.merge! formatted_hash
+      Paperclip::Attachment.default_options.inspect
+    rescue Errno::ENOENT => e
+      puts 'didnt load the paperclip file :( '
+      #do nothing ...
+    end
