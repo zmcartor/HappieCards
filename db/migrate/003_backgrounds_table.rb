947fdf8337cb64f5d7c17b775e2daca7b84b7cfb
Zach <zmcartor@gmail.com>
2013-02-11 22:27:01 -0500

    Paperclip fields for background migration

--- db/migrate/003_backgrounds_table.rb
+++ db/migrate/003_backgrounds_table.rb
@@ -1,11 +1,11 @@
 class BackgroundsTable < ActiveRecord::Migration
  def change
     create_table :backgrounds do |t|
-
-      t.string :url
       t.references :category
-
       t.timestamps
+      t.string :pic_content_type
+      t.string :pic_file_name
+      t.string :pic_file_size
     end
   end
 end
